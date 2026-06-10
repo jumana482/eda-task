df9e9e83acfcb953139b3a13a1ddbac5e66e51aa
Your Name <you@example.com>
2026-06-10 12:13:24 +0530

    Added graphs

--- eda.py
+++ eda.py
@@ -16,6 +16,7 @@ print(df.isnull().sum())
 plt.figure(figsize=(8,6))
 sns.heatmap(df.corr(numeric_only=True), annot=True, cmap="coolwarm")
 plt.title("Correlation Heatmap")
+plt.savefig("heatmap.png")
 plt.show()
 
 # Histogram
